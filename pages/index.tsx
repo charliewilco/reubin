3d06c48e42490e7bcf278586486cc71dcc9a187a
Charles ⚡ <charlespeters42@gmail.com>
2020-10-01 16:14:21 -0700

    removing button

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const IndexPage = () => (
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Reubin is a Feedbin client for Android.
             </p>
-            <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+            {/* <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
                 <a
                   href="#"
@@ -40,7 +40,7 @@ const IndexPage = () => (
                   Get It Now
                 </a>
               </div>
-            </div>
+            </div> */}
           </div>
         </main>
       </div>
