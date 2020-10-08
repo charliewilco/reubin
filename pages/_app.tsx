93de588d20be2a9236b97f0b84b5b241f4e89125
Charles ⚡ <charlespeters42@gmail.com>
2020-10-08 08:40:10 -0700

    adding analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
+import { useEffect } from "react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import "../styles/index.css";
 
 export default function RootApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load("MIGXNOZX", {
+      includedDomains: ["rebuin.app"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
