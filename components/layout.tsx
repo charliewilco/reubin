4b1f40acb61a4756524213ac496088e4bdd1dedb
Charles ⚡ <charlespeters42@gmail.com>
2021-01-29 08:14:33 -0800

    favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import { Banner } from "./banner";
 import { DownloadBlock } from "./download";
 import { IconHeader } from "./icon-header";
 import { Footer } from "./footer";
+import Head from "next/head";
 
 export const MarketingLayout: React.FC<{ title?: string }> = ({
   title,
@@ -10,6 +11,9 @@ export const MarketingLayout: React.FC<{ title?: string }> = ({
   return (
     <>
       <Banner />
+      <Head>
+        <link rel="shortcut icon" href="favicon.png" />
+      </Head>
       <IconHeader title={title} />
 
       <main className="px-2 max-w-4xl mx-auto">{children}</main>
