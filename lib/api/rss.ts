bbbb1ed023873be7a97f5504559cae9ef943b181
Charles ⚡ <charlespeters42@gmail.com>
2021-02-27 22:48:28 -0800

    adding more data layers

--- lib/api/rss.ts
+++ lib/api/rss.ts
@@ -7,17 +7,18 @@ export class RSS extends Parser {
   }
   async getItems(url: string): Promise<IItem[]> {
     const feed = await this.parseURL(url);
+    console.log(feed);
     return feed.items.map((_f) => {
       return {
         id: 1,
-        created_at: _f.isoDate,
+        created_at: _f.isoDate!,
         title: _f.title ?? "...",
         content: _f.content!,
         feed_id: 2,
         author: _f.creator ?? "No Creator",
         summary: _f.contentSnippet ?? null,
         url: _f.link!,
-        published: _f.pubDate,
+        published: _f.pubDate!,
         extracted_content_url: _f.enclosure?.url ?? "...",
       };
     });
