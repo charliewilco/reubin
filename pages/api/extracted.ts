0cdf424a34bff285adfe797a0fce66bae69a62a0
Charlie ⚡ <4588060+charliewilco@users.noreply.github.com>
2021-10-09 07:55:45 -0700

    santizing extracted html

--- pages/api/extracted.ts
+++ pages/api/extracted.ts
@@ -1,9 +1,14 @@
 import Mercury from "@postlight/mercury-parser";
 import { NextApiHandler } from "next";
 import { reubin } from "../../lib/api";
+import { getContent } from "../../lib/html";
 
 const handler: NextApiHandler<Mercury.ParseResult> = async (req, res) => {
   const result = await Mercury.parse(reubin.urlQueryToString(req.query.url));
+
+  if (result.content !== null) {
+    result.content = getContent(result.content);
+  }
 
   res.json(result);
 };
