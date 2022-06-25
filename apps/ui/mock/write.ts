4dd749066a5bbe8b57ea9faf7e08e067d3933c31
Charlie ⚡ <charliewilc0@protonmail.com>
2022-06-24 21:58:22 -0700

    adding testing to CI

--- apps/ui/mock/write.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import fs from "fs";
-
-export const writeToMock = (name: string, content: any) => {
-  const f = path.join(process.cwd(), "mock", name.concat(".json"));
-
-  fs.writeFileSync(f, JSON.stringify(content, null, 2), "utf8");
-};
