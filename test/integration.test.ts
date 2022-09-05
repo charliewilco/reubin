36279cc3e351b5d2aa3bef5c3e5d3e1f6070a26a
Charlie ⚡ <charliewilc0@protonmail.com>
2022-09-04 23:09:43 -0700

    adding basic test for adding feeds

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,7 +1,52 @@
-import { resolver } from "../server/resolvers";
+/**
+ * @jest-environment node
+ */
+
+import { createServer } from "@graphql-yoga/common";
+import { schema } from "../server/schema";
+import { context } from "../server/context";
+import gql from "graphql-tag";
+
+const yoga = createServer({
+	schema: schema,
+	context: () => context,
+});
 
 describe("Integration", () => {
-	xit("has resolvers", () => {
-		console.log(resolver);
+	it("can create feeds", async () => {
+		const createFeed = await yoga.inject({
+			document: gql`
+				mutation CreateFeed($url: String!) {
+					addFeed(url: $url) {
+						id
+						title
+					}
+				}
+			`,
+			variables: {
+				url: "https://www.inputmag.com/rss",
+			},
+		});
+
+		expect(createFeed.executionResult?.data.addFeed.title).toEqual("Input");
+
+		const entryList = await yoga.inject({
+			document: gql`
+				query EntriesByFeed($id: ID!) {
+					entries(feed_id: $id) {
+						title
+						content
+						id
+						unread
+						published
+					}
+				}
+			`,
+			variables: {
+				id: createFeed.executionResult?.data.addFeed.id,
+			},
+		});
+
+		expect(entryList.executionResult?.data.entries.length).toBeGreaterThan(1);
 	});
 });
