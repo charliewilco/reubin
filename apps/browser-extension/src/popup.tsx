90e58cef7c25652d3e1235c2ece45a5c32e3e003
Charlie ⚡ <charliewilc0@protonmail.com>
2022-10-02 17:45:17 -0700

    no more jsx in the extension, removed source maps so the compile time went from 150kb to 13.42kb

--- apps/browser-extension/src/popup.tsx
+++
@@ -1,9 +0,0 @@
-import { h, render } from "preact";
-import { parseDocumentLinks } from "./lib/parse-document";
-import { AvailableFeedList } from "./components/available-feed-list";
-
-const root = document.getElementById("app");
-
-if (root !== null) {
-  render(<AvailableFeedList onSearch={parseDocumentLinks} />, root);
-}
