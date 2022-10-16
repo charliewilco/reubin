be25332d31012fdd08ba6d18ee8c9e524a4457ba
Charlie ⚡ <charliewilc0@protonmail.com>
2022-10-15 23:51:11 -0700

    root for workspaces

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -1,15 +1,15 @@
 import type { IGraphQLProject } from "graphql-config";
 
 const config: IGraphQLProject = {
-  schema: "apps/graphql/src/schema.graphql",
+  schema: "graphql/src/schema.graphql",
   extensions: {
     codegen: {
       generates: {
-        "./apps/graphql/src/__generated__.ts": {
+        "./graphql/src/__generated__.ts": {
           plugins: ["typescript", "typescript-resolvers"],
         },
-        "./apps/ui/src/lib/__generated__.ts": {
-          documents: "apps/ui/src/lib/*.graphql",
+        "./ui/src/lib/__generated__.ts": {
+          documents: "ui/src/lib/*.graphql",
           plugins: ["typescript", "typescript-operations", "typescript-graphql-request"],
         },
       },
