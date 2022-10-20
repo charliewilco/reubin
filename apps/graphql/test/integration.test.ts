1a7a57510f50cdc7e99cd3bc1c9c99e46a6707d1
Charlie ⚡ <charliewilc0@protonmail.com>
2022-10-19 23:23:39 -0700

    adding auth to the integration test

--- apps/graphql/test/integration.test.ts
+++ apps/graphql/test/integration.test.ts
@@ -4,11 +4,39 @@
 
 import { createApp } from "../src/app";
 import gql from "graphql-tag";
-
+import base64 from "base-64";
 import { createMercuriusTestClient } from "mercurius-integration-testing";
+
+let authToken: string | null = null;
 
 describe("Integration", () => {
   const client = createMercuriusTestClient(createApp());
+
+  test("can create users", async () => {
+    const createUser = await client.query(
+      gql`
+        mutation Register($email: String!, $password: String!) {
+          createUser(email: $email, password: $password) {
+            token
+            user {
+              id
+              email
+            }
+          }
+        }
+      `,
+      {
+        variables: {
+          email: "test@charlieisamazing.com",
+          password: base64.encode("P@ssw0rd"),
+        },
+      }
+    );
+
+    authToken = createUser.data.createUser.token;
+
+    expect(createUser.data.createUser.token).not.toBeNull();
+  });
 
   test("can create feeds", async () => {
     const createFeed = await client.query(
@@ -23,6 +51,9 @@ describe("Integration", () => {
       {
         variables: {
           url: "https://www.inputmag.com/rss",
+        },
+        headers: {
+          authorization: authToken ?? "",
         },
       }
     );
