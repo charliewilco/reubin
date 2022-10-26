d3516dea8d60621127aff9bdac6d9eeae5182ea5
Charlie ⚡ <charliewilc0@protonmail.com>
2022-10-26 03:43:48 -0700

    login tests

--- apps/ui/src/components/auth-forms.tsx
+++ apps/ui/src/components/auth-forms.tsx
@@ -75,7 +75,7 @@ export function LoginForm() {
       <div>
         <button
           type="submit"
-          disabled={isSubmitting}
+          disabled={isSubmitting || Object.keys(errors).length > 0}
           className="flex w-full justify-center rounded-md border border-transparent bg-sky-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">
           Login
         </button>
--- apps/ui/test/login.test.tsx
+++ apps/ui/test/login.test.tsx
@@ -1,7 +1,56 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "jotai";
+import { graphql } from "msw";
+import { setupServer } from "msw/node";
 import { LoginForm } from "../src/components/auth-forms";
+import { LoginMutation } from "../src/lib/__generated__";
+
+const loginHandler = jest.fn();
+
+const server = setupServer(
+  graphql.mutation<LoginMutation>("Login", (_req, res, ctx) => {
+    loginHandler(_req.variables);
+
+    return res(
+      ctx.data({
+        login: {
+          token: "valid-token",
+          user: {
+            id: "1",
+            email: _req.variables.email,
+          },
+        },
+      })
+    );
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+});
+afterAll(() => server.close());
+
+const routerPush = jest.fn();
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+      push: routerPush,
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    };
+  },
+}));
 
 describe("Login", () => {
   test("should display 'Login' as the button", () => {
@@ -28,5 +77,29 @@ describe("Login", () => {
     await userEvent.tab();
 
     expect(screen.getByText("Invalid email")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+
+  test("should respond with correct credentials", async () => {
+    render(
+      <Provider>
+        <LoginForm />
+      </Provider>
+    );
+
+    const emailInput = screen.getByTestId("login-email-input");
+    const passwordInput = screen.getByTestId("login-password-input");
+    await userEvent.type(emailInput, "matt@test.com");
+
+    await userEvent.type(passwordInput, "P@ssw0rd");
+
+    await userEvent.click(screen.getByRole("button"));
+
+    expect(loginHandler).toBeCalledWith({
+      email: "matt@test.com",
+      // the encoded version of "P@ssw0rd"
+      password: "UEBzc3cwcmQ=",
+    });
+    expect(routerPush).toHaveBeenCalledWith("/dashboard");
   });
 });
