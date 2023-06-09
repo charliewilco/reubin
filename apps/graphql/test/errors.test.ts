82aa8c1ea230cb7eb46eafa4be749fb7462e2626
Charlie ⚡ <charliewilc0@pm.me>
2023-06-09 16:28:05 -0700

    removing graphql server

--- apps/graphql/test/errors.test.ts
+++
@@ -1,9 +0,0 @@
-import { SDKError } from "../src/errors";
-
-describe("Errors", () => {
-	test("error contains message", () => {
-		const error = new SDKError("Service unavailable", 503);
-		expect(error).toBeInstanceOf(SDKError);
-		expect(error.name).toBe("SDKError");
-	});
-});
