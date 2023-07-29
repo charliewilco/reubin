bca6b89c2d9b2c84698aeb845e87778be6aaaed7
Charlie ⚡ <charliewilc0@pm.me>
2023-07-28 18:08:54 -0700

    incorporating server action validations

---
+++ src/utils/errors.ts
@@ -0,0 +1,110 @@
+import z from "zod";
+import { isNonEmptyArray, joinPath } from "./array";
+
+export class ValidationError extends Error {
+	details: Array<z.ZodIssue>;
+	name: "ZodValidationError";
+
+	constructor(message: string, details: Array<z.ZodIssue> | undefined = []) {
+		super(message);
+		this.details = details;
+		this.name = "ZodValidationError";
+	}
+
+	toString(): string {
+		return this.message;
+	}
+}
+
+function fromZodIssue(
+	issue: z.ZodIssue,
+	issueSeparator: string,
+	unionSeparator: string
+): string {
+	if (issue.code === "invalid_union") {
+		return issue.unionErrors
+			.reduce<string[]>((acc, zodError) => {
+				const newIssues = zodError.issues
+					.map((issue) => fromZodIssue(issue, issueSeparator, unionSeparator))
+					.join(issueSeparator);
+
+				if (!acc.includes(newIssues)) {
+					acc.push(newIssues);
+				}
+
+				return acc;
+			}, [])
+			.join(unionSeparator);
+	}
+
+	if (isNonEmptyArray(issue.path)) {
+		// handle array indices
+		if (issue.path.length === 1) {
+			const identifier = issue.path[0];
+
+			if (typeof identifier === "number") {
+				return `${issue.message} at index ${identifier}`;
+			}
+		}
+
+		return `${issue.message} at "${joinPath(issue.path)}"`;
+	}
+
+	return issue.message;
+}
+
+export type ZodError = z.ZodError;
+export type FromZodErrorOptions = {
+	maxIssuesInMessage?: number;
+	issueSeparator?: string;
+	unionSeparator?: string;
+	prefixSeparator?: string;
+	prefix?: string;
+};
+
+export function fromZodError(
+	zodError: ZodError,
+	options: FromZodErrorOptions = {}
+): ValidationError {
+	const {
+		maxIssuesInMessage = 99,
+		issueSeparator = "; ",
+		unionSeparator = ", or ",
+		prefixSeparator = ": ",
+		prefix = "Validation error",
+	} = options;
+
+	const reason = zodError.errors
+		// limit max number of issues printed in the reason section
+		.slice(0, maxIssuesInMessage)
+		// format error message
+		.map((issue) => fromZodIssue(issue, issueSeparator, unionSeparator))
+		// concat as string
+		.join(issueSeparator);
+
+	const message = reason ? [prefix, reason].join(prefixSeparator) : prefix;
+
+	return new ValidationError(message, zodError.errors);
+}
+
+export const toValidationError =
+	(options: Parameters<typeof fromZodError>[1] = {}) =>
+	(err: unknown): ValidationError | Error => {
+		if (err instanceof z.ZodError) {
+			return fromZodError(err, options);
+		}
+
+		if (err instanceof Error) {
+			return err;
+		}
+
+		return new Error("Unknown error");
+	};
+
+export function isValidationError(err: unknown): err is ValidationError {
+	return err instanceof ValidationError;
+}
+
+export function isValidationErrorLike(err: unknown): err is ValidationError {
+	return err instanceof Error && err.name === "ZodValidationError";
+}
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -39,3 +39,29 @@ export function zodToFieldErrors<T>(error: Zod.ZodError<T>): Partial<Record<keyo
 
 	return fieldErrors;
 }
+
+export async function parseFormData<T>(
+	formData: FormData,
+	schema: Zod.ZodSchema<T>
+): Promise<T> {
+	const object: any = {};
+	for (let [key, value] of formData.entries()) {
+		object[key] = value;
+	}
+	return schema.parse(object);
+}
+
+type Action<T> = (formData: FormData) => T;
+
+export async function validatedAction<T>(action: Action<T>, schema: Zod.ZodSchema<T>) {
+	return async (formData: FormData) => {
+		const validatedData: any = parseFormData(formData, schema);
+		const newFormData = new FormData();
+
+		for (const key in validatedData) {
+			newFormData.append(key, validatedData[key]);
+		}
+
+		return action(newFormData);
+	};
+}
