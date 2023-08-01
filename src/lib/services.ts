fbef8c35fa1803e86c8fc2c0b79acef8a631eee7
Charlie ⚡ <charliewilc0@pm.me>
2023-07-31 23:21:59 -0700

    using external package

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -2,23 +2,24 @@ import { cookies } from "next/headers";
 
 import { Auth, type AuthUserSession } from "./auth";
 import { Email } from "./email";
-import { EntrySearch } from "./search";
 import { prisma } from "./orm";
 import { RSSKit } from "./rss";
-import { PurifyHTML } from "./purify-html";
 import { Payments } from "./payments";
 import { redis } from "./kv";
 
 export class Services {
 	static mail = new Email();
 	static auth = Auth;
-	static search = new EntrySearch();
 	static rss = new RSSKit();
 	static db = prisma;
 	static kv = redis;
-	static htmlPurify = PurifyHTML.create();
 	static payments = new Payments();
 
+	/**
+	 * @deprecated Moved to `Controllers.session.getUserSession`
+	 * @param request
+	 * @returns
+	 */
 	static async getUserSession(request?: Request): Promise<AuthUserSession> {
 		let authRequest = this.auth.handleRequest({
 			request,
